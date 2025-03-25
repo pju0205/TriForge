466e22792d9c350d9459e5908b0ed3def5a0cee3
minsupanda <104746519+minsupanda@users.noreply.github.com>
2025-03-25 14:26:41 +0900

    2025.03.25 움직임 구현중
    Motion Matching 구현중
    Walk 절뚝거림
    Run -> Walk 딜레이 (Run이 좀 더 출력됨)

---
+++ Source/TriForge/Private/TFMyAnimInstance.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TFMyAnimInstance.h"
+#include "TFMyCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CharacterTrajectoryComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "KismetAnimationLibrary.h"
+
+UTFMyAnimInstance::UTFMyAnimInstance()
+{
+	Direction = 0;
+	GroundSpeed = 0;
+	IsJump = false;
+	IsCrouching = false;
+}
+
+void UTFMyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	MyCharacter = Cast<ATFMyCharacter>(TryGetPawnOwner());
+	if (MyCharacter != nullptr)
+	{
+		MyCharacterMovement = MyCharacter->GetCharacterMovement();
+	}
+}
+
+void UTFMyAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+	if (MyCharacter)
+	{
+		CharacterTrajectory = MyCharacter->GetCharacterTrajectoryComponent();
+		Direction = UKismetAnimationLibrary::CalculateDirection(MyCharacter->GetVelocity(), MyCharacter->GetActorRotation());
+		GroundSpeed = UKismetMathLibrary::VSizeXY(MyCharacter->GetVelocity());
+		IsJump = MyCharacterMovement->IsFalling();
+		IsCrouching = MyCharacterMovement->IsCrouching();
+	}
+}
--- Source/TriForge/TriForge.Build.cs
+++ Source/TriForge/TriForge.Build.cs
@@ -8,7 +8,7 @@ public class TriForge : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "MotionTrajectory", "AnimGraphRuntime" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
