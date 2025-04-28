dbe7d72bb5059637e277dfd71c1ffabb51c64986
choiguri <cutwoplus@naver.com>
2025-04-28 15:03:34 +0900

    25.04.28 HTTP, DATA
    - HTTTP 관련 파일 만들어두기
    - DATA Asset 만들어서 HTTP 준비
    - GameplayTags 사용

---
+++ Source/DedicatedServers/Private/Data/API/APIData.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/API/APIData.h"
+
+FString UAPIData::GetAPIEndpoint(const FGameplayTag& APIEndpoint)
+{
+	const FString ResourceName = Resources.FindChecked(APIEndpoint);
+	return InvokeURL + "/" + Stage + "/" + ResourceName;
+}
---
+++ Source/DedicatedServers/Public/Data/API/APIData.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTags/DedicatedServersTags.h"
+#include "APIData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UAPIData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	FString GetAPIEndpoint(const FGameplayTag& APIEndpoint);
+
+protected:
+
+	// Name of this API - 데이터 자산에서 라벨링을 위한 것으로, 어떤 코드에서도 사용되지 않음
+	UPROPERTY(EditDefaultsOnly)
+	FString Name;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FString InvokeURL;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString Stage;
+
+
+	// 열거형으로 만드는 것도 좋은 선택이지만
+	// gameplay Tag로 만드는게 더 유용하다.
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FGameplayTag, FString> Resources;
+};
